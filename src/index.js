9402169a26cd0ab6e189da91adee2fc11a9a77a2
Ivan Brkljacic <ivan.brkljacic@arsfutura.co>
2021-02-10 21:36:46 +0100

    Add eslint import sort.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import '~/styles/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '~/styles/index.css';
+
 import App from '~/components/App';
 
 ReactDOM.render(
